6872dfff1a288f0b34e3c1eee14e98dad0be7867
kwagyoungsan <61777583+kwagyoungsan@users.noreply.github.com>
2020-12-29 22:36:08 +0900

    2D Game(Upload - 3)
    - Add footstep sound
    - Add background music function

---
+++ BGMManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+
+    static public BGMManager instance;
+
+    public AudioClip[] clips;
+
+    private AudioSource source;
+
+    private WaitForSeconds waitTime = new WaitForSeconds(0.01f);
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void Play(int _playMusicTrack)
+    {
+        source.clip = clips[_playMusicTrack];
+        source.Play();
+    }
+
+    public void Stop()
+    {
+        source.Stop();
+    }
+
+    public void Pause()
+    {
+        source.Pause();
+    }
+
+    public void UnPause()
+    {
+        source.UnPause();
+    }
+
+    public void SetVolume(float _volume)
+    {
+        source.volume = _volume;
+    }
+    IEnumerator FadeOutMusicCoroutine()
+    {
+        for(float i = 1; i >= 0f; i -= 0.01f)
+        {
+            source.volume =i;
+            yield return waitTime;
+        }
+    }
+    public void FadeOutMusic()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeOutMusicCoroutine());
+    }
+
+    IEnumerator FadeInMusicCoroutine()
+    {
+        for (float i = 0; i <= 1f; i += 0.01f)
+        {
+            source.volume = i;
+            yield return waitTime;
+        }
+    }
+
+    public void FadeInMusic()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeInMusicCoroutine());
+    }
+
+    
+}
---
+++ TestScript_2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript_2 : MonoBehaviour
+{
+    BGMManager BGM;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM = FindObjectOfType<BGMManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(abc());
+    }
+
+    IEnumerator abc()
+    {
+        BGM.FadeOutMusic();
+        yield return new WaitForSeconds(3f);
+        BGM.FadeInMusic();
+    }
+}
