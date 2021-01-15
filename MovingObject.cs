aa58f02d8ae7b58292ef2afdc47c3a6ff3108c08
kwagyoungsan <61777583+kwagyoungsan@users.noreply.github.com>
2021-01-16 02:09:17 +0900

    2D Game (Upload - 4)

--- MovingObject.cs
+++ MovingObject.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
+
+    public string characterName;
+
     public BoxCollider2D boxCollider;
     public LayerMask layerMask;
 
@@ -13,43 +16,69 @@ public class MovingObject : MonoBehaviour
     protected int currentWalkcount;
     public Animator animator;
 
-    protected void Move(string _dir)
+    protected bool NPCcanMove;
+
+    public Queue<string> queue;
+    public void Move(string _dir,int _frequency=5)
     {
-        StartCoroutine(MoveCoroutine(_dir));
+        StartCoroutine(MoveCoroutine(_dir, _frequency));
     }
 
-    IEnumerator MoveCoroutine(string _dir)
+    IEnumerator MoveCoroutine(string _dir, int _frequency)
     {
-        vector.Set(0, 0, vector.z);
-
-        switch (_dir)
+       while(queue.Count != 0)
         {
-            case "UP":
-                vector.y = 1f;
-                break;
-            case "DOWN":
-                vector.y = -1f;
-                break; 
-            case "LEFT":
-                vector.x = -1f;
-                break;
-            case "RIGHT":
-                vector.x = 1f;
-                break;
-        }
-        animator.SetFloat("DirX", vector.x);
-        animator.SetFloat("DirY", vector.y);
-        animator.SetBool("Walking", true);
+            string direction = queue.Dequeue();
+            NPCcanMove = false;
+            vector.Set(0, 0, vector.z);
 
-        while (currentWalkcount < walkCount)
-        {
-            transform.Translate(vector.x * speed, vector.y * speed, 0);
-            currentWalkcount++;
-            yield return new WaitForSeconds(0.01f);
+            switch (direction)
+            {
+                case "UP":
+                    vector.y = 1f;
+                    break;
+                case "DOWN":
+                    vector.y = -1f;
+                    break;
+                case "LEFT":
+                    vector.x = -1f;
+                    break;
+                case "RIGHT":
+                    vector.x = 1f;
+                    break;
+            }
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+            animator.SetBool("Walking", true);
+
+            while (currentWalkcount < walkCount)
+            {
+                transform.Translate(vector.x * speed, vector.y * speed, 0);
+                currentWalkcount++;
+                yield return new WaitForSeconds(0.01f);
+            }
+            currentWalkcount = 0;
+            if (_frequency != 5)
+                animator.SetBool("Walking", false);
+            NPCcanMove = true;
         }
-        currentWalkcount = 0;
         animator.SetBool("Walking", false);
-      
+    }
+
+    protected bool CheckCollision()
+    {
+        RaycastHit2D hit;
+        Vector2 Start = transform.position;
+        Vector2 End = Start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount);
+
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(Start, End, layerMask);
+        boxCollider.enabled = true;
+
+        if (hit.transform != null)
+            return true;
+
+        return false;
     }
 }
 
---
+++ PlayerManager.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MovingObject
+{
+    static public PlayerManager instance;
+    public string currentMapName; // tranferMapName 스크립트에 있는 transferMapName 변수 값을 저장
+
+    public float runSpeed;
+    private float ApplyrunSpeed;
+    private bool applyRunFlag;
+
+    private bool canMove = true;
+
+    public string walkSound_1;
+    public string walkSound_2;
+    public string walkSound_3;
+    public string walkSound_4;
+
+    private AudioManager theAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            queue = new Queue<string>();
+            instance = this;
+            DontDestroyOnLoad(this.gameObject); // 객체가 파괴되지 않고 저장
+            animator = GetComponent<Animator>();
+            theAudio = FindObjectOfType<AudioManager>();
+            boxCollider = GetComponent<BoxCollider2D>();
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        while (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                ApplyrunSpeed = runSpeed;
+                applyRunFlag = true;
+            }
+            else
+            {
+                ApplyrunSpeed = 0;
+                applyRunFlag = false;
+            }
+
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            if (vector.x != 0)
+                vector.y = 0;
+
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+
+            bool checkCollisionFlag = base.CheckCollision();
+            if (checkCollisionFlag)
+                break;
+
+            animator.SetBool("Walking", true);
+
+            int temp = Random.Range(1, 4);
+            switch (temp)
+            {
+                case 1:
+                    theAudio.Play(walkSound_1);
+                    break;
+                case 2:
+                    theAudio.Play(walkSound_2);
+                    break;
+                case 3:
+                    theAudio.Play(walkSound_3);
+                    break;
+                case 4:
+                    theAudio.Play(walkSound_4);
+                    break;
+            }
+
+            while (currentWalkcount < walkCount)
+            {
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + ApplyrunSpeed), 0, 0);
+                }
+
+                else
+                {
+                    transform.Translate(0, vector.y * (speed + ApplyrunSpeed), 0);
+                }
+                if (applyRunFlag)
+                    currentWalkcount++;
+                currentWalkcount++;
+                yield return new WaitForSeconds(0.01f);
+
+
+            }
+            currentWalkcount = 0;
+
+        }
+        animator.SetBool("Walking", false);
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
+            }
+        }
+
+    }
+}
